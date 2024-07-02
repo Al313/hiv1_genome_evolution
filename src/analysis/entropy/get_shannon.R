b2275721fd86d5b517f88647ce49c4244f1cfdaf
Ali Movasati <alimos313@Alis-Laptop.local>
2024-07-02 17:56:15 +0200

    1st manuscript analyses scripts

--- src/analysis/entropy/get_shannon.R
+++ src/analysis/entropy/get_shannon.R
@@ -17,6 +17,8 @@ if (length(args) == 0){
     cl_line <- args[1]
 }
 
+print(cl_line)
+
 
 
 if (dir.exists("/Users/alimos313")){
@@ -91,10 +93,10 @@ for (exp in c("EXPIII", "EXPIV")){
         for (psg in sort(unique(variants_sub$passage))){
 
             variants_sub3 <- variants_sub2[variants_sub2$passage == psg,]
-            variants_sub3 <- variants_sub3[variants_sub3$genomic_pos >= 454 & variants_sub3$genomic_pos <= 9626,]
+            variants_sub3 <- variants_sub3[variants_sub3$genomic_pos >= 454 & variants_sub3$genomic_pos <= 9625,]
 
             for (i in seq(454,9626)){
-                
+                print(i)
                 if (i %in% variants_sub3$genomic_pos){
                     ss <- variants[variants$exp_line == line & variants$passage == psg & variants$genomic_pos == i,]
                     alt_shannon <- sum(sapply(as.numeric(ss$allele_freq), FUN = shannon))
@@ -114,6 +116,6 @@ for (exp in c("EXPIII", "EXPIV")){
 colnames(shannon_df) <- c("experiment", "exp_line", "passage", "genomic_position", "shannon_entropy")
 
 
-write.table(shannon_df, file = paste0("/home/amovas/data/genome-evo-proj/results/tables/misc/", cl_line,"_shannon_entropy.tsv"), quote = F, row.names = F, sep = "\t")
+write.table(shannon_df, file = paste0(main_wd, "genome-evo-proj/results/tables/misc/", cl_line,"_shannon_entropy.tsv"), quote = F, row.names = F, sep = "\t")
 
 
