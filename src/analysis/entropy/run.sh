2f576cc7b14c7b3429fd88667da8e7a655301abd
Ali Movasati <ali.movasatt@gmail.com>
2025-05-14 10:09:37 +0200

    diversity measures

--- src/analysis/entropy/run.sh
+++ src/analysis/entropy/run.sh
@@ -19,7 +19,7 @@ echo "#!/bin/bash
 #SBATCH --array=13-20
 #SBATCH -c 1
 #SBATCH --time=3-00:00:00
-#SBATCH --mem=10G	
+#SBATCH --mem=15G	
 #SBATCH --output=${out_dir}/%x-%j-%a.out
 
 echo \$SLURM_ARRAY_TASK_ID
